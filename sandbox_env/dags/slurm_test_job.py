17c944d62f69a584c1612a8fb11e91ef8b0261d8
Unai Motriko Gomez <umotriko@CPU00646U.vicomtech.es>
2025-04-09 13:15:10 +0200

    Updates plugins to better parse output and conditional logic on trigger

---
+++ sandbox_env/dags/slurm_test_job.py
@@ -0,0 +1,13 @@
+from airflow import DAG
+from datetime import datetime
+from operators.slurm_operator import SubmitAndMonitorSlurmJobOperator
+
+with DAG("slurm_async_job", start_date=datetime(2024, 1, 1), schedule_interval=None, catchup=False, tags=["SLURM"]) as dag:
+    run_job = SubmitAndMonitorSlurmJobOperator(
+        task_id='submit_slurm',
+        ssh_conn_id='hpc_ssh',
+        qos='qos_di14',
+        name='nuevotest',
+        script_path='example_run.sh',
+        poll_interval=60
+    )
